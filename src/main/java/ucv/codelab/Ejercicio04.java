20d19e4b3707122da4ea99209c5d36daf48aa0e1
Wil PJ <147434623+WilderPascual16@users.noreply.github.com>
2024-09-07 18:31:46 -0500

    Add files via upload

---
+++ src/main/java/ucv/codelab/Ejercicio04.java
@@ -0,0 +1,39 @@
+package ucv.codelab;
+import javax.swing.JOptionPane;
+public class Ejercicio04 {
+ // Método iniciar
+    public static void iniciar() {
+         // Solicitar el ingreso del sueldo básico y el cargo
+         String sueldoBasicoStr = JOptionPane.showInputDialog("Ingrese el sueldo básico:");
+         String cargo = JOptionPane.showInputDialog("Ingrese el cargo del trabajador:"+
+                                                     "\n"+"O para Obrero"+
+                                                     "\n"+"E para Empleado"+
+                                                     "\n"+"J para Ejecutivo");
+ 
+         // Convertir el sueldo básico a número
+         double sueldoBasico = Double.parseDouble(sueldoBasicoStr);
+         double bonificacion = 0, asignacion = 0, refrigerio = 0;
+ 
+         // Calcular los beneficios según el cargo
+         if (cargo.equalsIgnoreCase("O")) {
+             bonificacion = 100;
+             asignacion = 120;
+         } else if (cargo.equalsIgnoreCase("E")) {
+             bonificacion = 120;
+             asignacion = 150;
+             refrigerio = 200;
+         } else if (cargo.equalsIgnoreCase("J")) {
+             bonificacion = 250;
+             asignacion = 500;
+             refrigerio = 250;
+         } else {
+             JOptionPane.showMessageDialog(null, "CARGO NO VÁLIDO."+
+                                            "\n"+":) BYE BYE :)");
+             System.exit(0);
+         }
+         // Calcular el total de los ingresos
+         double total = sueldoBasico + bonificacion + asignacion + refrigerio;
+         // Mostrar el resultado
+         JOptionPane.showMessageDialog(null, "Total de ingresos: " + total);
+     }
+}
