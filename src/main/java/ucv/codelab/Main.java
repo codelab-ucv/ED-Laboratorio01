1250fea1f1f408491ce0fcfd0516a5d3699c55d0
Madgen Del Castillo <mcastillocho@ucvvirtual.edu.pe>
2024-09-06 19:35:15 -0500

    Inicio del Proyecto

---
+++ src/main/java/ucv/codelab/Main.java
@@ -0,0 +1,29 @@
+package ucv.codelab;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        String respuesta = JOptionPane.showInputDialog(null, """
+                INGRESE EL EJERCICIO A EJECUTAR
+                ================================
+                Ejercicio01  [ 1 ]
+                Ejercicio02  [ 2 ]
+                Ejercicio03  [ 3 ]
+                Ejercicio04  [ 4 ]
+                Ejercicio05  [ 5 ]
+                """, "Laboratorio01", 3);
+        int opcion = Integer.parseInt(respuesta);
+        switch (opcion) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            default:
+                break;
+        }
+        JOptionPane.showMessageDialog(null, "Programa finalizado.\nAdios.");
+
+    }
+}
